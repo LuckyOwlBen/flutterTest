ca298736a675973912b19c92e45f32372c1dd0bf
Ben <benmalmberg2@gmail.com>
2024-06-05 18:12:52 -0700

    struggling with getFacts method

--- flutter_test_project/lib/app/services/api_service.dart
+++ flutter_test_project/lib/app/services/api_service.dart
@@ -2,8 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:flutter/material.dart';
 
 class ApiService extends ChangeNotifier {
-  final String _baseUrl =
-      'https://dl.dropboxusercontent.com/s/2iodh4vg0eortkl/facts.json';
+  final String _baseUrl = 'http://localhost:5000/unrelatedMethod';
   final Dio _dio = Dio();
   FeedResponse? feedData;
 
@@ -12,16 +11,12 @@ class ApiService extends ChangeNotifier {
   }
 
   void getFacts() async {
-    _dio.get(_baseUrl).asStream().map((response) => {
-          feedData = FeedResponse()
-            ..title = response.data['title']
-            ..images = (response.data['rows'] as List)
-                .map((image) => ImageData()
-                  ..title = image['title']
-                  ..description = image['description']
-                  ..imageHref = image['imageHref'])
-                .toList()
-        });
+    Response<FeedResponse> response = await _dio.get(_baseUrl,
+        options: Options(
+          responseType: ResponseType.stream,
+        ));
+    feedData = response.data;
+    notifyListeners();
   }
 }
 
